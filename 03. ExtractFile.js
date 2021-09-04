36b57c3b4497c79c218779e80d6bd6e3faec8a82
Dani <yordana_ivanova@abv.bg>
2021-09-04 15:00:16 +0300

    initial commit

---
+++ 03. ExtractFile.js
@@ -0,0 +1,16 @@
+function solve(path){
+
+    let tokens = path.split('\\')
+    let fileName = tokens.pop()
+
+    let index = fileName.lastIndexOf('.')
+    let name = fileName.substring(0, index)
+    let ext = fileName.substring(index + 1)
+
+    console.log(`File name: ${name}`)
+    console.log(`File extension: ${ext}`)
+
+
+}
+
+solve('C:\\Internal\\training-internal\\Template.pptx')
